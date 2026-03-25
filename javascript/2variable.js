2e563d2eda7368c3c4c81d03197e1a2faef687ad
akashsingh2199 <ak4646037@gmail.com>
2026-03-25 11:20:58 +0530

    add

---
+++ javascript/2variable.js
@@ -0,0 +1,10 @@
+const accountId=1234
+let accemail = "a@gmail.com"
+var accpass = "1111" //var ab use nhi hota hai mostly
+acccity = "ktr" //ye ho sakta ha but use nhi karna h
+accemail ="b@mail.com" //way of change
+accpass ="2222"
+acccity ="pur"
+//accountId ="3434" // const not change
+let accdate; //";" use kar bhi sakte hai nhi bhi//vale is undefined
+console.table([accountId, accemail, accpass,acccity ,accdate]);
---
+++ javascript/3 datatype.js
@@ -0,0 +1,12 @@
+"use strict";//treat new version of js
+//alert (3+3) //in browser work, not here in node
+console.log(5+6);
+
+//number
+//bigint
+//string = ""
+//Boolean 
+//null
+//undefined
+//symbol = unique
+//object
