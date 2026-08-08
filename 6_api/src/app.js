0098a7a6e0014e2c247597541a6ba7a28ea54b78
akashsingh2199 <ak4646037@gmail.com>
2026-08-09 01:13:23 +0530

    spelling error

--- 6_api/src/app.js
+++ 6_api/src/app.js
@@ -24,8 +24,8 @@ app.delete('/notes/:id', (req, res) => {
 
 app.patch('/notes/:id', (req, res) => {
   const id = req.params.id; 
-  const description = req.body.description;
-  notes[id].description = description;
+  const description = req.body.discription;
+  notes[id].discription = description;
   res.status(200).json({ message: 'Note updated successfully' });
 }); 
 module.exports = app;
